97cab64bc578618200ecbf4f5ce6a2a2b41571e6
Joakim kartveit <joakim.kartveit@nav.no>
2018-09-10 15:07:36 +0200

    Byttet ut ediloggid med smId

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -71,7 +71,7 @@ suspend fun listen(consumer: KafkaConsumer<String, String>, applicationState: Ap
             val msgHead: XMLMsgHead = fellesformat.get()
             val mottakEnhetBlokk: XMLMottakenhetBlokk = fellesformat.get()
             val marker = append("msgId", msgHead.msgInfo.msgId)
-                    .and<LogstashMarker>(append("ediLoggId", mottakEnhetBlokk.ediLoggId))
+                    .and<LogstashMarker>(append("smId", mottakEnhetBlokk.ediLoggId))
                     .and<LogstashMarker>(append("organizationNumber", msgHead.msgInfo.sender.organisation.extractOrganizationNumber()))
             log.info(marker, "Received a SM2013, trying to persist in Joark")
         }
