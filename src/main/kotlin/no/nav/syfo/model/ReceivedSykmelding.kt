cf723966315072ddf23351654a89146024def9c4
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-11-28 13:22:47 +0100

    Correct identation for ReceivedSykmelding

--- src/main/kotlin/no/nav/syfo/model/ReceivedSykmelding.kt
+++ src/main/kotlin/no/nav/syfo/model/ReceivedSykmelding.kt
@@ -4,18 +4,18 @@ import no.nav.helse.sm2013.HelseOpplysningerArbeidsuforhet
 import java.time.LocalDateTime
 
 data class ReceivedSykmelding(
-        val sykmelding: HelseOpplysningerArbeidsuforhet,
-        val aktoerIdPasient: String,
-        val aktoerIdLege: String,
-        val navLogId: String,
-        val msgId: String,
-        val legekontorOrgNr: String,
-        val legekontorOrgName: String,
-        val mottattDato: LocalDateTime,
-        val signaturDato: LocalDateTime,
-        /**
-         * Full fellesformat as a XML payload, this is only used for infotrygd compat and should be removed in thefuture
-         */
-        @Deprecated("Only used for infotrygd compat, will be removed in the future")
-        val fellesformat: String
+    val sykmelding: HelseOpplysningerArbeidsuforhet,
+    val aktoerIdPasient: String,
+    val aktoerIdLege: String,
+    val navLogId: String,
+    val msgId: String,
+    val legekontorOrgNr: String,
+    val legekontorOrgName: String,
+    val mottattDato: LocalDateTime,
+    val signaturDato: LocalDateTime,
+    /**
+     * Full fellesformat as a XML payload, this is only used for infotrygd compat and should be removed in thefuture
+     */
+    @Deprecated("Only used for infotrygd compat, will be removed in the future")
+    val fellesformat: String
 )
