9bd4cd7d2c32fa6358735f87450d201eea30e2b7
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-03-07 09:08:16 +0100

    Don't log headers

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -76,7 +76,7 @@ data class ApplicationState(var running: Boolean = true, var initialized: Boolea
 @KtorExperimentalAPI
 val httpClient = HttpClient(CIO) {
     install(Logging) {
-        level = LogLevel.HEADERS
+        level = LogLevel.INFO
     }
     install(JsonFeature) {
         serializer = JacksonSerializer {
