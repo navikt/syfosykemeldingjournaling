38bf83d3d42994c87741158a52e9ddeec341846e
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-03-15 15:49:50 +0100

    Make orgnr optional

--- src/main/kotlin/no/nav/syfo/model/OpprettSak.kt
+++ src/main/kotlin/no/nav/syfo/model/OpprettSak.kt
@@ -17,7 +17,7 @@ data class OpprettSakResponse(
     val id: Long,
     val tema: String,
     val aktoerId: String,
-    val orgnr: String,
+    val orgnr: String?,
     val fagsakNr: String,
     val applikasjon: String,
     val opprettetAv: String,
