7c729e26829ec4f257a97dab926538348cc44eba
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-12 17:31:49 +0200

    Made field fagsakNr optional

--- src/main/kotlin/no/nav/syfo/model/OpprettSak.kt
+++ src/main/kotlin/no/nav/syfo/model/OpprettSak.kt
@@ -18,7 +18,7 @@ data class OpprettSakResponse(
     val tema: String,
     val aktoerId: String,
     val orgnr: String?,
-    val fagsakNr: String,
+    val fagsakNr: String?,
     val applikasjon: String,
     val opprettetAv: String,
     val opprettetTidspunkt: ZonedDateTime
