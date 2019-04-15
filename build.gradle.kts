a00993edafb39967880cc5989e9c5380bf87fae2
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-15 14:48:42 +0200

    Bumped version to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
-version = "1.0.1"
+version = "1.0.2"
 
 val confluentVersion = "5.0.0"
 val coroutinesVersion = "1.1.1"
