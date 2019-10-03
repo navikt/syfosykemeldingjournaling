6e892cfee31d34b6f4423a1151b6688ac3ec36cb
Joakim Kartveit <joakimkartveit@gmail.com>
2019-10-03 17:43:40 +0200

    Legger til sjekk at det er gyldig fnr evt dnr

---
+++ src/test/kotlin/no/nav/syfo/ValidatePersonNumberSpek.kt
@@ -0,0 +1,38 @@
+package no.nav.syfo
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import no.nav.syfo.validation.validatePersonAndDNumber
+import no.nav.syfo.validation.validatePersonAndPersonDNumberRange
+import org.amshove.kluent.shouldEqual
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+val personNumberDateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("ddMMyy")
+
+object ValidateDNumberSpek : Spek({
+    describe("Testing validation personNumber") {
+        it("Should check validate as fnr") {
+            val generateFnr = generatePersonNumber(LocalDate.of(1991, 1, 1), false)
+            val validFnr = validatePersonAndPersonDNumberRange(generateFnr)
+            validFnr shouldEqual true
+        }
+
+        it("Should check validate as d-number") {
+            val generateDnumber = generatePersonNumber(LocalDate.of(1991, 1, 1), true)
+            val validdnumber = validatePersonAndPersonDNumberRange(generateDnumber)
+            validdnumber shouldEqual true
+        }
+    }
+})
+
+fun generatePersonNumber(bornDate: LocalDate, useDNumber: Boolean = false): String {
+    val personDate = bornDate.format(personNumberDateFormat).let {
+        if (useDNumber) "${it[0] + 4}${it.substring(1)}" else it
+    }
+    return (if (bornDate.year >= 2000) (75011..99999) else (11111..50099))
+            .map { "$personDate$it" }
+            .first {
+                validatePersonAndDNumber(it)
+            }
+}
