9a95814fb398472909d542abd817424e32d8a968
Joakim Kartveit <joakimkartveit@gmail.com>
2019-10-30 14:21:26 +0100

    Fjerner try catch

--- src/main/kotlin/no/nav/syfo/service/JournalService.kt
+++ src/main/kotlin/no/nav/syfo/service/JournalService.kt
@@ -15,7 +15,6 @@ import no.nav.syfo.model.ValidationResult
 import no.nav.syfo.sak.avro.RegisterJournal
 import no.nav.syfo.util.LoggingMeta
 import no.nav.syfo.util.wrapExceptions
-import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonPersonIkkeFunnet
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -25,7 +24,6 @@ class JournalService(private val env: Environment, private val producer: KafkaPr
     suspend fun onJournalRequest(receivedSykmelding: ReceivedSykmelding, validationResult: ValidationResult, loggingMeta: LoggingMeta) {
         wrapExceptions(loggingMeta) {
             log.info("Mottok en sykmelding, prover aa lagre i Joark {}", StructuredArguments.fields(loggingMeta))
-            try {
                 val patient = fetchPerson(personV3, receivedSykmelding.personNrPasient, loggingMeta)
 
                 val pdfPayload = createPdfPayload(receivedSykmelding, validationResult, patient)
@@ -51,8 +49,6 @@ class JournalService(private val env: Environment, private val producer: KafkaPr
                 log.info("Melding lagret i Joark med journalpostId {}, {}",
                         journalpost.journalpostId,
                         StructuredArguments.fields(loggingMeta))
-            } catch (hentPersonPersonIkkeFunnet: HentPersonPersonIkkeFunnet) {
-            }
         }
     }
 }
